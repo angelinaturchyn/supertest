f8e5940ea5a5881c79732d28c94ec8c9a9e07639
Angelin Turchyn <aturchyn@100MBP-ATURCHYN.local>
2022-07-24 22:59:59 -0500

    grabbed token

--- firstTest.js
+++ firstTest.js
@@ -6,8 +6,23 @@ const foxtrotLogin = async()=> {
     .send({ email: "aturchyn@foxtrotco.com", password: "1234567" }) // sends a JSON post body
     .set('Contetnt-Type', 'application/json')
     console.log(dashboard.text)
+
+    let userToken = dashboard.body.user.api_token
+    console.log(userToken)
     //dashboard gets the whole json body
     //dashboard.text gets only html
+
+    let rewards = await superagent
+    .get('https://staging.api.foxtrotchicago.com/v5/rewards/107756/')
+    .send(userToken)
+    console.log(rewards)
+    .then(res => {
+        // res.body, res.headers, res.status
+     })
+     .catch(err => {
+        // err.message, err.response
+     });
+    
 }
 
 foxtrotLogin()
