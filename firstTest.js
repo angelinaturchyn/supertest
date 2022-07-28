b06910dd1b2a3ee31106cdd21315a6c11de3a642
Angelin Turchyn <aturchyn@100MBP-ATURCHYN.local>
2022-07-27 22:09:04 -0500

    modified perks balance

--- firstTest.js
+++ firstTest.js
@@ -1,27 +1,29 @@
 const superagent = require('superagent').agent();
 
+let userToken;
+let userID;
+
 const foxtrotLogin = async()=> {
-    let dashboard = await superagent
+    let response = await superagent
     .post('https://staging.api.foxtrotchicago.com/v5/login')
     .send({ email: "aturchyn@foxtrotco.com", password: "1234567" }) // sends a JSON post body
     .set('Contetnt-Type', 'application/json')
-    console.log(dashboard.text)
+    console.log(response.text)
 
-    let userToken = dashboard.body.user.api_token
+    userToken = response.body.user.api_token
+    userID = response.body.user.id
     console.log(userToken)
+    console.log(userID)
     //dashboard gets the whole json body
     //dashboard.text gets only html
 
-    let rewards = await superagent
-    .get('https://staging.api.foxtrotchicago.com/v5/rewards/107756/')
-    .send(userToken)
-    console.log(rewards)
-    .then(res => {
-        // res.body, res.headers, res.status
-     })
-     .catch(err => {
-        // err.message, err.response
-     });
+    let perks = await superagent
+    .get(`https://staging.api.foxtrotchicago.com/v5/rewards/${userID}/`)
+    .set('Authorization', 'Bearer 58380|w9yMhLYtniLFYWIQJxyGIY0OKYGXKbAbOTVuJirX')
+    console.log(perks.text)
+    
+    let rewardsBalance = perks.body.rewards.balance;
+    console.log(rewardsBalance)
     
 }
 
